89ab95652ac3ce8b3b879dc5f705528a78da6aee
soOwasTaken <444deph12@gmail.com>
2022-12-06 14:57:53 +0000

    fixing ISO C90 on cd.c

--- cd.c
+++ cd.c
@@ -6,30 +6,29 @@
  */
 int cd(char **args)
 {
+	/* If no directory is specified, go to the user's home directory. */
+	char *home_dir = getenv("HOME");
+
+	if (home_dir == NULL)
+	{
+		perror("cd");
+		return (0);
+	}
+
+	/* Use chdir to change the current working directory. */
 	if (args[1] == NULL)
 	{
-		/* If no directory is specified, go to the user's home directory. */
-		char *home_dir = getenv("HOME");
-
-		if (home_dir == NULL)
+		if (chdir(home_dir) != 0)
 		{
 			perror("cd");
-			return (0);
 		}
-
-		/* Use execvp to execute the "cd" command with the specified directory. */
-		char *cmd = "cd";
-		char *argv[] = {cmd, home_dir, NULL};
-
-		execvp(cmd, argv);
 	}
 	else
 	{
-		/* Use execvp to execute the "cd" command with the specified directory. */
-		char *cmd = "cd";
-		char *argv[] = {cmd, args[1], NULL};
-
-		execvp(cmd, argv);
+		if (chdir(args[1]) != 0)
+		{
+			perror("cd");
+		}
 	}
 
 	return (0);
